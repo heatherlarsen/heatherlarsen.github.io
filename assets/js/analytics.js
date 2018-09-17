8a47dd604a9fc019517a492c571a9783398c9b8e
Heather Larsen <heatherlarsenux@gmail.com>
2018-09-17 11:01:30 -0700

    adding google analytics

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,1 +1,18 @@
 // analytics.js
+
+function initAnalytics() {
+	if (debug) console.log('initAnalytics()');
+
+	
+	$('.js-ga').on('click', function() {
+		// check if element has analytics data attribute
+		if ($(this).data('ga')) {
+
+			var data = $(this).data('ga').split(',');
+			var category = $.trim(data[0]);
+			var label = $.trim(data[1]);
+
+			ga('send', 'event', category, 'click', label);
+		}
+	});
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -18,6 +18,8 @@ $(function() {
 		initFilters();
 	}
 
+	initAnalytics();
+
 	/*
 	if (pageType === "project") {
 		initProjectNav();
